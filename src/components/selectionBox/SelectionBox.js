5864e76c37b7591109b36adafb1c93e7ec110482
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-11-19 16:26:10 -0500

    feature: highlight selected box green

--- src/components/selectionBox/SelectionBox.js
+++ src/components/selectionBox/SelectionBox.js
@@ -1,18 +1,39 @@
 import React from 'react';
 
-import './Styles.scss';
+import styles from './Styles.scss';
 
 const SelectionBox = (props) => {
-  return(
+  const [selected, setSelected] = React.useState({ status: false, id: -1 });
+
+  const handleClick = (event) => {
+    console.log('i fired');
+    setSelected({
+      status: !selected.status,
+      id: selected.id === -1 ? props.id : -1,
+    });
+  };
+
+  return (
     <div
       className="selectionBox"
-      id={"selectionBox" + props.id}
+      id={'selectionBox' + props.id}
+      style={{
+        backgroundColor: selected.id === props.id ? styles.grassBlade : 'white',
+      }}
     >
-      <img className="selectionBox--image" alt={props.answer.imageAlt} src={props.answer.image} />
-      <input className="selectionBox--checkbox" type="checkbox" />
+      <img
+        className="selectionBox--image"
+        alt={props.answer.imageAlt}
+        src={props.answer.image}
+      />
+      <input
+        className="selectionBox--checkbox"
+        type="checkbox"
+        onClick={handleClick}
+      />
       <span className="selectionBox--text">{props.answer.text}</span>
     </div>
-   )
-}
+  );
+};
 
 export default SelectionBox;
